cac39a07caa8cbd900f83d3c918d735db95df029
Macarini74 <iagomacarini@alunos.utfpr.edu.br>
2025-06-09 14:01:09 -0300

    Criação dos arquivos

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from data.CriacaoBD import criarTable
+
+def main():
+    criarTable()
+    st.set_page_config(
+        page_title="Dashboard",
+        page_icon="🏠",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
+    dashboard_page = st.Page("frontend/dashboard.py", title="Dashboard", icon="🏠", url_path="/dashboard")
+    aeroportos = st.Page("frontend/aeroportos.py", title="Aeroportos", icon="👥", url_path="/aeroportos") 
+    empresas = st.Page("frontend/empresas.py", title="empresas", icon="💪", url_path="/empresas") 
+    eficiencia = st.Page("frontend/eficiencia.py", title="eficiencia", icon="💰", url_path="/eficiencia")
+
+    pg = st.navigation([dashboard_page])
+
+    pg = st.navigation([
+            dashboard_page,
+            aeroportos,
+            empresas,
+            eficiencia
+        ])
+    
+    pg.run()
+
+if __name__ == "__main__":
+    main()
