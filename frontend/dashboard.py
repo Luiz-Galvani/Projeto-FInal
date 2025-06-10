33fb5911a6485a4d26f3ed79baf0c6e43582251e
Macarini74 <iagomacarini@alunos.utfpr.edu.br>
2025-06-10 12:09:26 -0300

    Gráficos de Linhas Prontos

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -54,33 +54,105 @@ st.divider()
 
 ## Evolução Temporal (Gráfico de Linhas)
 # Passageiros pagantes e gratuitos
-cursor.execute(''' 
-    CREATE VIEW IF NOT EXISTS passageiros_por_mes AS
-        SELECT
-            mes,
-            SUM(passageiros_pagos + passageiros_gratis) AS total_passageiros
-        FROM
-            voos
-        GROUP BY
-            mes
-        ORDER BY
-            mes
-''')
 
-# Carga paga e correio
+col1, col2 = st.columns(2)
+meses = {
+    1: 'Janeiro',
+    2: 'Fevereiro',
+    3: 'Março',
+    4: 'Abril',
+    5: 'Maio',
+    6: 'Junho',
+    7: 'Julho',
+    8: 'Agosto',
+    9: 'Setembro',
+    10: 'Outubro',
+    11: 'Novembro',
+    12: 'Dezembro'
+}
+
+with col1.container(border=True):
+    tab1, tab2, tab3 = st.tabs(['Passageiros x Mês', 'Carga x Mês', 'Correio x Mês'])
+    with tab1:
+        cursor.execute(''' 
+            CREATE VIEW IF NOT EXISTS passageiros_por_mes AS
+                SELECT
+                    mes,
+                    SUM(passageiros_pagos + passageiros_gratis) AS total_passageiros
+                FROM
+                    voos
+                GROUP BY
+                    mes
+                ORDER BY
+                    mes
+        ''')
+        cursor.execute('SELECT * FROM passageiros_por_mes')
+        column_names = [description[0] for description in cursor.description]
+        rows = cursor.fetchall()
+
+        df_pass = pd.DataFrame(rows, columns=column_names)
+        df_pass['mes_nome'] = df_pass['mes'].map(meses)
+        fig = px.line(df_pass,
+                    x='mes_nome',
+                    y='total_passageiros',
+                    labels={'mes_nome': 'Mês', 'total_passageiros': 'Total de Passageiros'})
+        st.plotly_chart(fig)
+
+    with tab2:
+        # Carga paga e correio
+        cursor.execute('''
+                        CREATE VIEW IF NOT EXISTS carga_correio AS
+                            SELECT
+                                mes,
+                                SUM(carga_paga_kg) AS total_carga
+                            FROM
+                                voos
+                            GROUP BY
+                                mes
+                            ORDER BY
+                                mes               
+        ''')
+        cursor.execute('SELECT * FROM carga_correio')
+        column_names = [description[0] for description in cursor.description]
+        rows = cursor.fetchall()
+
+        df_carga = pd.DataFrame(rows, columns=column_names)
+        df_carga['mes_nome'] = df_carga['mes'].map(meses)
+
+        fig = px.line(df_carga,
+                    x='mes_nome',
+                    y='total_carga',
+                    labels={'mes_nome': 'Mês', 'total_carga': 'Carga Total (Kg)'}
+                    )
+        st.plotly_chart(fig)
+    
+    with tab3:
+        cursor.execute('''
+                        CREATE VIEW IF NOT EXISTS correio AS
+                            SELECT
+                                mes,
+                                SUM(correio_kg) AS total_correio
+                            FROM
+                                voos
+                            GROUP BY
+                                mes
+                            ORDER BY
+                                mes               
+        ''')   
+        cursor.execute('SELECT * FROM correio')
+        column_names = [description[0] for description in cursor.description]
+        rows = cursor.fetchall()
+
+        df_correio = pd.DataFrame(rows, columns=column_names)
+        df_correio['mes_nome'] = df_correio['mes'].map(meses)     
+
+        fig = px.line(df_correio,
+                    x='mes_nome',
+                    y='total_correio',
+                    labels={'mes_nome': 'Mês', 'total_correio': 'Correio Total (Kg)'}
+                    )
+        st.plotly_chart(fig)
 
 ## Dristribuição de Natures dos Voos (Gráfico de Pizza)
 # Doméstico vs Internacional 
 
-
-# col1.container(border=True).metric("Total de Passageiros:", f"{int(total_passageiros)}")
-
-# col2.container(border=True).metric("Destino Mais Procurado: ", f'{destino_procurado}')
-
-# col3.container(border=True).metric("Taxa de Ocupação:", f"{taxa_ocupacao:.2%}")
-
-# col4.container(border=True).metric("Empresa com mais voos:", f'{empresa_mais_voos}')
-
-# st.divider()
-
-# col1, col2 = st.columns(2)
