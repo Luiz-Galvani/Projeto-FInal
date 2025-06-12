f63162e8aac4d20794f4f13ad1d5198cc234e0aa
Macarini74 <iagomacarini@alunos.utfpr.edu.br>
2025-06-12 13:59:40 -0300

    Atualizacao gitignore

--- data/CriacaoBD.py
+++ data/CriacaoBD.py
@@ -14,12 +14,8 @@ def criarTable():
     existing_columns_to_drop = [col for col in columns_to_drop if col in df.columns]
     if existing_columns_to_drop:
         df.drop(columns=existing_columns_to_drop, inplace=True)
-        print(f"Colunas {existing_columns_to_drop} removidas do DataFrame.")
-    else:
-        print("Nenhuma das colunas de UF/Região especificadas foi encontrada no DataFrame.")
 
     df.dropna(inplace=True)
-    print(f"Linhas com valores nulos removidas. Restam {len(df)} linhas.")
 
     conn = sqlite3.connect("data/voos.db")
     cursor = conn.cursor()
@@ -62,48 +58,9 @@ def criarTable():
         bagagem_kg REAL -- BAGAGEM (KG) pode ser real
     )
     ''')
+
     conn.commit()
-
-    df.rename(columns={
-        'EMPRESA (SIGLA)': 'empresa_sigla',
-        'EMPRESA (NOME)': 'empresa_nome',
-        'EMPRESA (NACIONALIDADE)': 'empresa_nacionalidade',
-        'ANO': 'ano',
-        'MÊS': 'mes',
-        'AEROPORTO DE ORIGEM (SIGLA)': 'origem_sigla',
-        'AEROPORTO DE ORIGEM (NOME)': 'origem_nome',
-        'AEROPORTO DE ORIGEM (PAÍS)': 'origem_pais',
-        'AEROPORTO DE ORIGEM (CONTINENTE)': 'origem_continente',
-        'AEROPORTO DE DESTINO (SIGLA)': 'destino_sigla',
-        'AEROPORTO DE DESTINO (NOME)': 'destino_nome',
-        'AEROPORTO DE DESTINO (PAÍS)': 'destino_pais',
-        'AEROPORTO DE DESTINO (CONTINENTE)': 'destino_continente',
-        'NATUREZA': 'natureza',
-        'GRUPO DE VOO': 'grupo_voo',
-        'PASSAGEIROS PAGOS': 'passageiros_pagos',
-        'PASSAGEIROS GRÁTIS': 'passageiros_gratis',
-        'CARGA PAGA (KG)': 'carga_paga_kg',
-        'CARGA GRÁTIS (KG)': 'carga_gratis_kg',
-        'CORREIO (KG)': 'correio_kg',
-        'ASK': 'ask',
-        'RPK': 'rpk',
-        'ATK': 'atk',
-        'RTK': 'rtk',
-        'COMBUSTÍVEL (LITROS)': 'combustivel_litros',
-        'DISTÂNCIA VOADA (KM)': 'distancia_voada_km',
-        'DECOLAGENS': 'decolagens',
-        'CARGA PAGA KM': 'carga_paga_km',
-        'CARGA GRATIS KM': 'carga_gratis_km',
-        'CORREIO KM': 'correio_km',
-        'ASSENTOS': 'assentos',
-        'PAYLOAD': 'payload',
-        'HORAS VOADAS': 'horas_voadas',
-        'BAGAGEM (KG)': 'bagagem_kg'
-    }, inplace=True)
-
     df.to_sql("voos", conn, if_exists="replace", index=False)
 
     conn.commit()
     conn.close()
-
-    print("Dados do CSV carregados e do CSV carregados e inseridos no banco de dados 'voos.db' no banco de dados 'voos.db' com sucesso!")
